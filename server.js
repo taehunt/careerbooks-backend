38fb7ff10b2d8f29b945348015c46db4b1519e98
taehunt <wxg005@naver.com>
2025-04-17 03:08:51 +0900

    업로드 파일 git에 포함

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
